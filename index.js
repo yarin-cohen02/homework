9bc1241079af30e192317e660104113806a9c8d5
Yarin Cohen <yarinco123@gmail.com>
2025-05-03 21:05:58 +0300

    start adding timers

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 const elevators = new Array(5).fill(null).map((value, i) => ({
     id: i,
     available: true,
-    currentFloor: 0
+    currentFloor: 0,
+    startTime: -1 // change startTime to be in each floor - maybe in executeQueue()?
 }));
 
 const queue = [];
@@ -25,14 +26,19 @@ const findElevator = (floor) => {
 const makeBusy = (id, floor) => {
     elevators[id].available = false;
     elevators[id].currentFloor = floor;
-    console.log(elevators);
+    elevators[id].startTime = Date.now();
 }
 
 const makeAvailable = (id) => {
     elevators[id].available = true;
+    elevators[id].startTime = -1;
 }
 
 const applyAnimation = async (id, floor) => {
+
+    // show timer
+    $(`#elv${id}-fl${floor}`).text("0 secs");
+    $(`elv${id}-fl${floor}`).show();
 
     // make elevator red
     $(`#elv${id}`).css("color", "#f04c4c");
@@ -50,6 +56,9 @@ const applyAnimation = async (id, floor) => {
     $(`#${floor}`).text("Arrived")
       .addClass("arrived-button")
       .removeClass("waiting-button");
+
+    // hide timer
+    $(`#elv${id}-fl${floor}`).hide();
   
     // make ekevator green
     $(`#elv${id}`).css("color", "#5bcd88");
@@ -89,10 +98,15 @@ const handleClick = (floor) => {
     $(`#${floor}`).text("Waiting").addClass("waiting-button").removeClass("call-button");
     queue.push(floor);
 }
+
+const refreshTimers = () => {
+  $(`#elv${id}-fl${floor}`).text(`${Math.floor((Date.now() - elevators[id].startTime) / 1000)} secs`);
+}
     
 
 // On start:
 
-setInterval(() => executeQueue(), 1000);
+setInterval(() => executeQueue(), 1000); 
+setInterval(() => refreshTimers(), 1000); 
 
 $("button").click((e) => {handleClick(e.target.id)});
