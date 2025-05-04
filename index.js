3dc5a09661bc175ffe850609fd7a78cf53cfa70f
Yarin Cohen <yarinco123@gmail.com>
2025-05-04 18:15:57 +0300

    final!!!

--- index.js
+++ index.js
@@ -1,124 +1,126 @@
-// REMAINING TODO:
-// - Add timer to show how long it took to arrive (elv0-fl2)
+  // Variables:
 
-// Variables:
+  const elevators = new Array(5).fill(null).map((value, i) => ({
+      id: i,
+      available: true,
+      currentFloor: 0,
+  }));
 
-const elevators = new Array(5).fill(null).map((value, i) => ({
-    id: i,
-    available: true,
-    currentFloor: 0,
-}));
-
-const queue = [];
-let activeRequests = [];
+  const queue = [];
+  let activeRequests = [];
 
 
-// Functions:
+  // Functions:
 
-const floorHeight = (x) => x * 62;
+  const floorHeight = (x) => x * 62;
 
-const findElevator = (floor) => {
-    return elevators.filter((a) => a.available)
-        .sort((a, b) => Math.abs(a.currentFloor - floor) - Math.abs(b.currentFloor - floor))[0]
-        .id;
-}
+  const findElevator = (floor) => {
 
-const makeBusy = (id, floor) => {
-    elevators[id].available = false;
-    elevators[id].currentFloor = floor;
-}
-
-const makeAvailable = (id) => {
-    elevators[id].available = true;
-    elevators[id].startTime = -1;
-}
-
-const applyAnimation = async (id, floor) => {
-
-    // show timer
-    $(`elv${id}-fl${floor}`).show();
-
-    // make elevator red
-    $(`#elv${id}`).css("color", "#f04c4c");
+    const bestElevator = elevators.filter((a) => a.available)
+      .sort((a, b) => Math.abs(a.currentFloor - floor) - Math.abs(b.currentFloor - floor))[0];
   
-    // move elevator
-    await new Promise(resolve => {
-      $(`#elv${id}`).animate({ bottom: floorHeight(floor) }, 2000, resolve);
-    });
+    return bestElevator ? bestElevator.id : -1;
+  }
 
-    // play sound
-    const dingSound = new Audio("elevator-ding.mp3");
-    dingSound.play();
-  
-    // change button to Arrived
-    $(`#${floor}`).text("Arrived")
-      .addClass("arrived-button")
-      .removeClass("waiting-button");
+  const makeBusy = (id, floor) => {
+      elevators[id].available = false;
+      elevators[id].currentFloor = floor;
+  }
 
-    // hide timer
-    $(`#elv${id}-fl${floor}`).hide();
-  
-    // make ekevator green
-    $(`#elv${id}`).css("color", "#5bcd88");
-  
-    // wait 2 secs
-    await new Promise(resolve => setTimeout(resolve, 2000));
-  
-    // change button back to Call
-    $(`#${floor}`).text("Call")
-      .addClass("call-button")
-      .removeClass("arrived-button");
-  
-    // make elevator black
-    $(`#elv${id}`).css("color", "black");
+  const makeAvailable = (id) => {
+      elevators[id].available = true;
+  }
 
-  };  
+  const applyAnimation = async (id, floor) => {
 
-const executeQueue = async () => {
+      // show timer
+      $(`#elv${id}-fl${floor}`).show();
 
-    for (let requestedFloor of queue) {
+      // make elevator red
+      $(`#elv${id}`).css("color", "#f04c4c");
+    
+      // move elevator
+      await new Promise(resolve => {
+        $(`#elv${id}`).animate({ bottom: floorHeight(floor) }, 2000, resolve);
+      });
+
+      // play sound
+      const dingSound = new Audio("elevator-ding.mp3");
+      dingSound.play();
+    
+      // change button to Arrived
+      $(`#${floor}`).text("Arrived")
+        .addClass("arrived-button")
+        .removeClass("waiting-button");
+
+      // hide timer
+      $(`#elv${id}-fl${floor}`).hide();
+    
+      // make ekevator green
+      $(`#elv${id}`).css("color", "#5bcd88");
+    
+      // wait 2 secs
+      await new Promise(resolve => setTimeout(resolve, 2000));
+    
+      // change button back to Call
+      $(`#${floor}`).text("Call")
+        .addClass("call-button")
+        .removeClass("arrived-button")
+        .prop("disabled", false);
+    
+      // make elevator black
+      $(`#elv${id}`).css("color", "black");
+
+    };  
+
+  const executeQueue = async () => {
+
+      if (!queue.length) return;
+
+      const requestedFloor = queue[0]
 
       const availableElevator = findElevator(requestedFloor);
       if (availableElevator === -1) return;
 
+      // add elevator assigned to the task
       const elevatorRequest = activeRequests.find(a => a.floor === requestedFloor);
-      if (elevatorRequest) elevatorRequest.elevator = availableElevator;
+      elevatorRequest.elevator = availableElevator;
 
       queue.shift();
       await makeBusy(availableElevator, requestedFloor);
       await applyAnimation(availableElevator, requestedFloor);
       await makeAvailable(availableElevator);
+      activeRequests = activeRequests.filter(a => a.floor !== requestedFloor);
 
-      activeRequests = activeRequests.filter(a => !(a.floor === requestedFloor && a.elevator === availableElevator));
-
-    }
-
-}
-
-const handleClick = (floor) => {
-
-    $(`#${floor}`).text("Waiting").addClass("waiting-button").removeClass("call-button");
-
-    activeRequests.push({
-      floor: floor,
-      elevator: -1,
-      startTime: Date.now()
-    });
-
-    queue.push(floor);
-
-}
-
-const refreshTimers = () => {
-  for (let activeReq of activeRequests) {
-    $(`#elv${activeReq.elevator}-fl${activeReq.floor}`).text(`${Math.floor((Date.now() - activeReq.startTime) / 1000)} secs`);
   }
-}
-    
 
-// On start:
+  const handleClick = (floor) => {
 
-setInterval(() => executeQueue(), 1000); 
-setInterval(() => refreshTimers(), 500); 
+      $(`#${floor}`).text("Waiting").addClass("waiting-button").removeClass("call-button").prop("disabled", true);
 
-$("button").click((e) => {handleClick(e.target.id)});
+      activeRequests.push({
+        floor: floor,
+        elevator: -1,
+        startTime: Date.now()
+      });
+
+      queue.push(floor);
+
+  }
+
+  const refreshTimers = () => {
+    for (let activeReq of activeRequests) {
+      $(`#elv${activeReq.elevator}-fl${activeReq.floor}`).text(`${Math.floor((Date.now() - activeReq.startTime) / 1000)} secs`);
+    }
+  }
+      
+
+  // On start:
+
+  setInterval(() => executeQueue(), 500); 
+  setInterval(() => refreshTimers(), 500); 
+
+  $("button").click((e) => {
+    if (e.target.disabled) return;
+    return handleClick(e.target.id);
+  });
